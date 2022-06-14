adcb8be4554186eadbc836a38260df0d162fdbd6
shortc1rcuit <declan@themcmoos.net>
2022-06-14 22:38:50 +0100

    Lexer complete

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,21 +3,22 @@ use itertools::Itertools;
 pub mod cluster;
 use cluster::*;
 
-pub fn lex_text(base_code: &str) -> Vec<Cluster> {
-    let centre_chars: Vec<usize> = get_centre_chars_loc(base_code);
+pub fn lex_text(base_code: &str) -> Result<Vec<Cluster>, &'static str> {
+    let base_code = strip_whitespace(base_code);
 
-    println!("{:#?}", centre_chars);
-    todo!();
+    let cluster_starts = get_centre_chars_loc(&base_code);
+    if cluster_starts.is_empty() {
+        return Err("No base characters");
+    }
+    let cluster_starts = pair(cluster_starts);
+
+    let unclustered = get_cluster_text(base_code, cluster_starts);
+
+    Ok(text_to_cluster(unclustered))
 }
 
-fn pair(unpaired: Vec<u32>) -> Vec<(u32, u32)>{
-    let mut paired = Vec::new();
-
-    for (a, b) in unpaired.into_iter().tuple_windows() {
-        paired.push((a, b));
-    }
-
-    paired
+fn strip_whitespace(a: &str) -> String {
+    a.chars().filter(|x| !x.is_whitespace()).collect()
 }
 
 fn get_centre_chars_loc(base_code: &str) -> Vec<usize> {
@@ -30,22 +31,123 @@ fn get_centre_chars_loc(base_code: &str) -> Vec<usize> {
         .collect()
 }
 
+fn pair(unpaired: Vec<usize>) -> Vec<(usize, Option<usize>)> {
+    let mut paired = Vec::new();
+    //Unwrap is fine as I've checked that unpaired has at least one value in it
+    let last = *unpaired.last().unwrap();
+
+    for (a, b) in unpaired.into_iter().tuple_windows() {
+        paired.push((a, Some(b)));
+    }
+
+    paired.push((last, None));
+    paired
+}
+
+fn get_cluster_text(base_code: String, cluster_starts: Vec<(usize, Option<usize>)>) -> Vec<String> {
+    let mut unclustered: Vec<String> = Vec::new();
+
+    //Runs through the pairs and gets the text between them
+    //If this is at the end of the text (signified by a None)
+    //Then just return the rest of the text
+    for (a, b) in cluster_starts {
+        let a = a + 1;
+        match b {
+            Some(x) => unclustered.push(base_code.chars().skip(a).take(x - a).collect()),
+            None => unclustered.push(base_code.chars().skip(a).collect()),
+        };
+    }
+
+    //If two non-diacritic characters are next to each other
+    //cluster_text will have an empty Vec
+    //This filters those out
+    unclustered.into_iter().filter(|x| !x.is_empty()).collect()
+}
+
+fn text_to_cluster(unclustered: Vec<String>) -> Vec<Cluster> {
+    unclustered
+        .into_iter()
+        .map(|x| x.chars().into_iter().map(|a| (a as u32) - 0x300).collect())
+        .map(Cluster::new)
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn centre_chars_loc() {
-        let base_code = "A\u{0310}\u{030F}\u{0306}B\u{031D}";
+    fn strip_whitespace_test() {
+        let a = "a b
+        c";
 
-        assert_eq!(get_centre_chars_loc(base_code), vec![0, 4]);
+        assert_eq!(strip_whitespace(a), "abc".to_string());
     }
 
     #[test]
-    fn pairs() {
+    fn centre_chars_loc_test() {
+        let base_code = "A\u{0310}\u{030F}\u{0306}B\u{031D}".to_string();
+
+        assert_eq!(get_centre_chars_loc(&base_code), vec![0, 4]);
+    }
+
+    #[test]
+    fn pairs_test() {
         let unpaired = vec![1, 2, 3, 4, 5];
-        let paired = vec![(1, 2), (2, 3), (3, 4), (4, 5)];
+        let paired = vec![
+            (1, Some(2)),
+            (2, Some(3)),
+            (3, Some(4)),
+            (4, Some(5)),
+            (5, None),
+        ];
 
         assert_eq!(pair(unpaired), paired);
+    }
+
+    #[test]
+    fn single_pair_test() {
+        let unpaired = vec![1];
+        let paired = vec![(1, None)];
+
+        assert_eq!(pair(unpaired), paired);
+    }
+
+    #[test]
+    fn cluster_text_test() {
+        let base_code = "A\u{0310}\u{030F}\u{0306}BC\u{031D}".to_string();
+        let cluster_starts = vec![(0, Some(4)), (4, Some(5)), (5, None)];
+        let unclustered = vec!["\u{0310}\u{030F}\u{0306}", "\u{031D}"];
+
+        assert_eq!(get_cluster_text(base_code, cluster_starts), unclustered);
+    }
+
+    #[test]
+    fn single_cluster_text_test() {
+        let base_code = "A\u{0310}\u{030F}\u{0306}".to_string();
+        let cluster_starts = vec![(0, None)];
+        let unclustered = vec!["\u{0310}\u{030F}\u{0306}"];
+
+        assert_eq!(get_cluster_text(base_code, cluster_starts), unclustered);
+    }
+
+    #[test]
+    fn text_to_cluster_test() {
+        let unclustered = vec![
+            "\u{0310}\u{030F}\u{0306}".to_string(),
+            "\u{031D}".to_string(),
+        ];
+        let clustered = vec![
+            Cluster {
+                top: vec![6, 0xF, 0x10],
+                bottom: vec![],
+            },
+            Cluster {
+                top: vec![],
+                bottom: vec![0x1D],
+            },
+        ];
+
+        assert_eq!(text_to_cluster(unclustered), clustered);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,14 @@ use cluster::*;
 use lexer::*;
 
 fn main() {
-    //Only the diacritics needed to print "Zalgo"
-    let unclustered = vec![
-        '̆', '̏', '̐', //Push o
-        '̆', '̇', '̐', //Push g
-        '̆', '̌', '̐', //Push l
-        '̆', '́', '̐', //Push a
-        '̅', '̊', '̐', //Push Z
-        '̝', '̝', '̝', '̝', '̝',
-    ]; //Print 5 chars
-
-    //Converts the characters to numbers to:
-    //A) Make characters easier to write in the program
-    //B) Make 0-F digits easier to use
-    let unclustered = unclustered
-        .into_iter()
-        .map(|x| (x as u32) - 0x300)
-        .collect();
-
-    let lexed_code = Cluster::new(unclustered);
+    let base_code = "A\u{0310}\u{030A} \u{0305}\u{0310}\u{0301}\u{0306}\u{0310}\u{030C}\u{031D}\u{0306}\u{031D}\u{031D}B\u{0310}\u{0307}\u{0306}\u{0310}\u{030F}\u{0306}\u{031D}\u{031D}";
+    let lexed_code = match lex_text(base_code) {
+        Ok(x) => x,
+        Err(e) => {
+            eprintln!("Compile Error! {}", e);
+            process::exit(1);
+        }
+    };
 
     if let Err(e) = interpret_code(lexed_code) {
         eprintln!("Runtime error! {}", e);
@@ -31,7 +20,7 @@ fn main() {
     }
 }
 
-fn interpret_code(lexed_code: Cluster) -> Result<(), &'static str> {
+fn interpret_code(lexed_code: Vec<Cluster>) -> Result<(), &'static str> {
     let mut stack: Vec<u32> = Vec::new();
 
     //Value used to build a number before it's pushed to the stack
@@ -39,34 +28,36 @@ fn interpret_code(lexed_code: Cluster) -> Result<(), &'static str> {
 
     let mut result = String::new();
 
-    for value in lexed_code.top {
-        if value <= 0xF {
-            //0-F value
-            pre_push = (pre_push << 4) + value;
-        } else if value == 0x10 {
-            //Push to stack
-            stack.push(pre_push);
-            pre_push = 0;
+    for cluster in lexed_code {
+        for value in cluster.top {
+            if value <= 0xF {
+                //0-F value
+                pre_push = (pre_push << 4) + value;
+            } else if value == 0x10 {
+                //Push to stack
+                stack.push(pre_push);
+                pre_push = 0;
+            }
         }
-    }
-    for value in lexed_code.bottom {
-        if value == 0x1D {
-            //Pop top of stack and print
-            let print_char = match stack.pop() {
-                Some(x) => x,
-                None => {
-                    return Err("Out of stack values!");
-                }
-            };
+        for value in cluster.bottom {
+            if value == 0x1D {
+                //Pop top of stack and print
+                let print_char = match stack.pop() {
+                    Some(x) => x,
+                    None => {
+                        return Err("Out of stack values!");
+                    }
+                };
 
-            let print_char = match char::from_u32(print_char) {
-                Some(x) => x,
-                None => {
-                    return Err("Invalid char value!");
-                }
-            };
+                let print_char = match char::from_u32(print_char) {
+                    Some(x) => x,
+                    None => {
+                        return Err("Invalid char value!");
+                    }
+                };
 
-            result = format!("{}{}", result, print_char);
+                result = format!("{}{}", result, print_char);
+            }
         }
     }
 
